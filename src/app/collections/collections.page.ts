16c34aa2fba0129dd171b44af1c0250b08e1868b
TiffanyYongNgikChee <G00425067@atu.ie>
2024-05-07 13:52:40 +0000

    first commit

--- src/app/collections/collections.page.ts
+++ src/app/collections/collections.page.ts
@@ -1,20 +1,35 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonCard, IonCardHeader, IonCardTitle, IonContent, IonHeader, IonLabel, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Storage } from '@ionic/storage-angular';
+import { RouterLinkWithHref } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-collections',
+  selector: 'app-collection',
   templateUrl: './collections.page.html',
   styleUrls: ['./collections.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [RouterLinkWithHref, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, IonLabel,IonContent,IonCard,IonCardHeader,IonCardTitle]
 })
 export class CollectionsPage implements OnInit {
+  
+  foodDetails:any[]=[];
 
-  constructor() { }
+  constructor(private storage: Storage,
+              private router:Router) { }
 
+  /*async ionViewWillEnter(){
+    await this.storage.create();
+    this.foodDetails = await this.storage.get('savedQuery') || [];
+    
+  }
+  */
+ 
   ngOnInit() {
   }
+
+  
 
 }
